9e17715d13a8e97e6a7dafb66a2885589160d7df
Akash Mondal <a98mondal@gmail.com>
2024-02-10 04:21:59 +0530

    gemi (voice): Add a new STT provider - wit.ai

--- api/listen.py
+++ api/listen.py
@@ -3,10 +3,14 @@ from fastapi import Depends, APIRouter, UploadFile
 from pydantic import BaseModel
 
 from stt.vosk import VoskRecognizer
+from stt.wit import WitRecognizer
 
 listen_api = APIRouter()
 
 @listen_api.post("/vosk")
 async def listen(file: UploadFile, vosk: Annotated[VoskRecognizer, Depends(VoskRecognizer)]):
-    file_type = file.content_type
+    return { "text": await vosk.listen(file) }
+
+@listen_api.post("/wit")
+async def listen(file: UploadFile, vosk: Annotated[WitRecognizer, Depends(WitRecognizer)]):
     return { "text": await vosk.listen(file) }
---
+++ stt/wit.py
@@ -0,0 +1,33 @@
+import asyncio
+import tempfile
+from fastapi import UploadFile
+from speech_recognition import Recognizer, AudioFile
+from os import getenv
+
+class WitRecognizer:
+    api_key: str
+    rec: Recognizer
+
+    def __init__(self):
+        self.api_key = getenv("WIT_AI_API_KEY")
+        self.rec = Recognizer()
+
+    async def convert_to_wav(self, audio_bytes: bytes, wav_path: str):
+        command = ['ffmpeg', '-i', '-', wav_path]
+        proc = await asyncio.create_subprocess_exec(
+            *command,
+            stdin=asyncio.subprocess.PIPE,
+            stdout=asyncio.subprocess.PIPE
+        )
+        await proc.communicate(input=audio_bytes)
+
+    async def listen(self, upload_file: UploadFile) -> str:
+        with tempfile.NamedTemporaryFile(suffix=".wav") as wav_file:
+            if ['audio/wav', 'audio/x-wav', 'audio/wave', 'audio/x-pn-wav'].__contains__(upload_file.content_type):
+                wav_file.write(upload_file.file.read())
+            else:
+                await self.convert_to_wav(upload_file.file.read(), wav_file.name)
+            wav_file.seek(0)
+            with AudioFile(wav_file) as audio_file:
+                audio_data = self.rec.record(audio_file)
+                return self.rec.recognize_wit(audio_data, key=self.api_key)
